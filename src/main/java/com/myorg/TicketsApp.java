12686440c4b34a160e0def45a77b6c6022dc6101
Alexander Kaserbacher <alexander.kaserbacher@tngtech.com>
2022-06-28 21:51:29 +0200

    remove TicketsStack since it is not needed

--- src/main/java/com/myorg/TicketsApp.java
+++ src/main/java/com/myorg/TicketsApp.java
@@ -1,13 +1,10 @@
 package com.myorg;
 
 import software.amazon.awscdk.App;
-import software.amazon.awscdk.StackProps;
 
 public class TicketsApp {
     public static void main(final String[] args) {
         App app = new App();
-
-        new TicketsStack(app, "TicketsStack", StackProps.builder().build());
 
         ConcertsStack.Builder.create(app, "ConcertsStack").build();
 
