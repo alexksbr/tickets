a7eb3eba040bbd7d470a47e784501ba86d5cbeba
Alexander Kaserbacher <alexander.kaserbacher@tngtech.com>
2022-06-29 22:25:00 +0200

    add service to cluster

--- infrastructure/src/main/java/com/myorg/infrastructure/ConcertsStack.java
+++ infrastructure/src/main/java/com/myorg/infrastructure/ConcertsStack.java
@@ -5,8 +5,7 @@ import software.amazon.awscdk.StackProps;
 import software.amazon.awscdk.services.ec2.InstanceClass;
 import software.amazon.awscdk.services.ec2.InstanceSize;
 import software.amazon.awscdk.services.ec2.InstanceType;
-import software.amazon.awscdk.services.ecs.AddCapacityOptions;
-import software.amazon.awscdk.services.ecs.Cluster;
+import software.amazon.awscdk.services.ecs.*;
 import software.constructs.Construct;
 
 public class ConcertsStack extends Stack {
@@ -17,14 +16,30 @@ public class ConcertsStack extends Stack {
     public ConcertsStack(final Construct scope, final String id, final StackProps props) {
         super(scope, id, props);
 
-        Cluster.Builder.create(this, "ConcertsServiceCluster")
-                .capacity(
-                        AddCapacityOptions.builder()
-                                .instanceType(
-                                        InstanceType.of(
-                                                InstanceClass.BURSTABLE2, InstanceSize.MICRO))
-                                .desiredCapacity(1)
-                                .build())
+        final Cluster cluster =
+                Cluster.Builder.create(this, "ConcertsServiceCluster")
+                        .capacity(
+                                AddCapacityOptions.builder()
+                                        .instanceType(
+                                                InstanceType.of(
+                                                        InstanceClass.BURSTABLE2,
+                                                        InstanceSize.MICRO))
+                                        .desiredCapacity(1)
+                                        .build())
+                        .build();
+
+        final Ec2TaskDefinition concertsServiceTaskDefinition =
+                Ec2TaskDefinition.Builder.create(this, "ConcertsServiceTaskDefinition").build();
+        concertsServiceTaskDefinition.addContainer(
+                "ConcertsServiceContainer",
+                ContainerDefinitionOptions.builder()
+                        .image(ContainerImage.fromAsset("../concerts-service"))
+                        .memoryLimitMiB(512)
+                        .build());
+
+        Ec2Service.Builder.create(this, "ConcertsService")
+                .cluster(cluster)
+                .taskDefinition(concertsServiceTaskDefinition)
                 .build();
     }
 }
