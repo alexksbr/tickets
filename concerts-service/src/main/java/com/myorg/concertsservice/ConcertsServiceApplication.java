44056d6243d21e1abb2dd158f86391a5b1ad333a
Alexander Kaserbacher <alexander.kaserbacher@tngtech.com>
2022-06-29 18:31:08 +0200

    add scaffold for concerts service

---
+++ concerts-service/src/main/java/com/myorg/concertsservice/ConcertsServiceApplication.java
@@ -0,0 +1,12 @@
+package com.myorg.concertsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcertsServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConcertsServiceApplication.class, args);
+    }
+}
---
+++ concerts-service/src/test/java/com/myorg/concertsservice/ConcertsServiceApplicationTests.java
@@ -0,0 +1,11 @@
+package com.myorg.concertsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcertsServiceApplicationTests {
+
+    @Test
+    void contextLoads() {}
+}
