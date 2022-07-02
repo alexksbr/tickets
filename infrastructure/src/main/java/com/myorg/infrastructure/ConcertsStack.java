956f2dcbce37ea643e58a8316ae43e39766bd158
Alexander Kaserbacher <alexander.kaserbacher@tngtech.com>
2022-07-02 11:26:07 +0200

    add load balancer

--- infrastructure/src/main/java/com/myorg/infrastructure/ConcertsStack.java
+++ infrastructure/src/main/java/com/myorg/infrastructure/ConcertsStack.java
@@ -1,11 +1,13 @@
 package com.myorg.infrastructure;
 
+import java.util.List;
 import software.amazon.awscdk.Stack;
 import software.amazon.awscdk.StackProps;
 import software.amazon.awscdk.services.ec2.InstanceClass;
 import software.amazon.awscdk.services.ec2.InstanceSize;
 import software.amazon.awscdk.services.ec2.InstanceType;
 import software.amazon.awscdk.services.ecs.*;
+import software.amazon.awscdk.services.ecs.patterns.ApplicationLoadBalancedEc2Service;
 import software.constructs.Construct;
 
 public class ConcertsStack extends Stack {
@@ -35,11 +37,36 @@ public class ConcertsStack extends Stack {
                 ContainerDefinitionOptions.builder()
                         .image(ContainerImage.fromAsset("../concerts-service"))
                         .memoryLimitMiB(512)
+                        .portMappings(List.of(PortMapping.builder().containerPort(8080).build()))
                         .build());
 
-        Ec2Service.Builder.create(this, "ConcertsService")
-                .cluster(cluster)
-                .taskDefinition(concertsServiceTaskDefinition)
-                .build();
+        final ApplicationLoadBalancedEc2Service concertsService =
+                ApplicationLoadBalancedEc2Service.Builder.create(this, "ConcertsService")
+                        .cluster(cluster)
+                        .taskDefinition(concertsServiceTaskDefinition)
+                        .build();
+
+        //        final Ec2Service ec2Service =
+        //                Ec2Service.Builder.create(this, "ConcertsService")
+        //                        .cluster(cluster)
+        //                        .taskDefinition(concertsServiceTaskDefinition)
+        //                        .desiredCount(1)
+        //                        .build();
+        //
+        //        final ApplicationLoadBalancer loadBalancer =
+        //                ApplicationLoadBalancer.Builder.create(this,
+        // "ConcertsServiceLoadBalancer")
+        //                        .vpc(cluster.getVpc())
+        //                        .internetFacing(true)
+        //                        .build();
+        //
+        //        final ApplicationListener applicationListener =
+        //                loadBalancer.addListener(
+        //                        "PublicListener",
+        // BaseApplicationListenerProps.builder().port(80).build());
+        //        applicationListener.addTargets(
+        //                "ECS",
+        //
+        // AddApplicationTargetsProps.builder().port(80).targets(List.of(ec2Service)).build());
     }
 }
